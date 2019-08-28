a7fa4d23e6da68047a0138de25e8350f97c59018
PCH <pch1024@outlook.com>
2019-08-28 23:59:39 +0800

    初始化

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,27 @@
 import './lib/polyfill';
 
-import * as ReactDOM from 'react-dom';
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { Wrapper } from './pages/wrapper';
-import './style/app.scss';
+import {ConfigProvider} from 'antd';
 
-import { DatePicker } from 'antd';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import zhCN from 'antd/es/locale-provider/zh_CN'; // 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import * as moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<Wrapper />, document.getElementById('app'));
+import './style/common.scss';
+
+import {Wrapper} from './pages/wrapper';
+
+moment.locale('zh-cn');
+
+const APP = () => {
+    return (
+        <ConfigProvider locale={zhCN}>
+            <Wrapper/>
+        </ConfigProvider>
+    )
+};
+
+ReactDOM.render(<APP/>, document.getElementById('app'));
