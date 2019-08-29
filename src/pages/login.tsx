0c0303d1897e0824a055f5d33bdd0c91d67771e1
pch <pch1024@outlook.com>
2019-08-29 17:48:37 +0800

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react';
 import '../style/login.scss';
-import {Form, Icon, Input, Button} from 'antd';
+import { Form, Icon, Input, Button, Row, Col, Carousel } from 'antd';
+
+import { Redirect } from 'react-router-dom';
 
 const iconEle = (name: string) => (
-    <Icon type={name} style={{color: 'rgba(0,0,0,.25)'}}/>
+    <Icon type={name} style={{ color: 'rgba(0,0,0,.25)' }} />
 );
 
-const FormEle = props => {
-    const {getFieldDecorator} = props.form;
+const FormEle = (props: any) => {
+    const { getFieldDecorator } = props.form;
+
+    const [code, setCode] = React.useState(updateCode());
 
     const rule = {
         username: {
             rules: [
                 {
                     required: true,
-                    message: 'Please input your username!',
+                    message: '请输入手机号或者邮箱',
                 },
             ],
         },
@@ -22,7 +26,7 @@ const FormEle = props => {
             rules: [
                 {
                     required: true,
-                    message: 'Please input your Password!',
+                    message: '请输入账户密码',
                 },
             ],
         },
@@ -30,16 +34,24 @@ const FormEle = props => {
             rules: [
                 {
                     required: true,
-                    message: 'Please input your code!',
+                    message: '请输入验证码',
                 },
             ],
         },
     };
 
-    function handleSubmit(e: React.FormEvent) {
+    function updateCode(): object {
+        return {
+            backgroundImage: `url(http://101.200.41.205:8080/captcha.php?${+new Date()})`,
+        };
+    }
+
+    function handleSubmit(e: React.FormEvent): void {
         e.preventDefault();
         props.form.validateFields((err: Error, values: Object) => {
             if (!err) console.log('表单输入值: ', values);
+
+            return <Redirect to='/home' />;
         });
     }
 
@@ -50,7 +62,7 @@ const FormEle = props => {
                     <Input
                         size='large'
                         prefix={iconEle('user')}
-                        placeholder='Username'
+                        placeholder='手机号或者邮箱'
                     />,
                 )}
             </Form.Item>
@@ -60,59 +72,117 @@ const FormEle = props => {
                         size='large'
                         prefix={iconEle('lock')}
                         type='password'
-                        placeholder='Password'
+                        placeholder='账户密码'
                     />,
                 )}
             </Form.Item>
             <Form.Item>
-                {getFieldDecorator('code', rule.code)(
-                    <Input
-                        size='large'
-                        placeholder="Enter your code"
-                        prefix={iconEle('key')}
-                        addonAfter={
-                            <img
-                                height='38px'
-                                src="http://101.200.41.205:8080/captcha.php"
-                                alt="code"
-                            />
+                <Row gutter={16}>
+                    <Col span={15}>
+                        {getFieldDecorator('code', rule.code)(
+                            <Input
+                                size='large'
+                                placeholder='验证码'
+                                prefix={iconEle('key')}
+                            />,
+                        )}
+                    </Col>
+                    <Col span={9}>
+                        {
+                            <Button
+                                size='large'
+                                className='code'
+                                type='link'
+                                onClick={() => setCode(updateCode())}
+                                style={code}></Button>
                         }
-                    />
-                )}
+                    </Col>
+                </Row>
             </Form.Item>
             <Form.Item>
                 <Button
-                    style={{width: '100%'}}
+                    style={{ width: '100%' }}
                     type='primary'
                     size='large'
                     htmlType='submit'
                     className='login-form-button'>
-                    Log in
+                    登 录
                 </Button>
             </Form.Item>
         </Form>
     );
 };
+
+const CarouselEle = () => {
+    const images = [
+        {
+            img: 'http://101.200.41.205:8080/images/redesign/1.png',
+            title: '全方位防护',
+            dec:
+                '独创的安全防护方案，通过开发语言层面深度、直接、快速的方式修复应用开发过程中产生的各类安全问题。',
+        },
+
+        {
+            img: 'http://101.200.41.205:8080/images/redesign/2.png',
+            title: '云智能补丁',
+            dec:
+                '发现应用特定补丁时，无须修改应用系统自身代码来修复应用系统自身的安全漏洞,让漏洞修复变的简单、快捷。',
+        },
+
+        {
+            img: 'http://101.200.41.205:8080/images/redesign/3.png',
+            title: '规则云同步',
+            dec:
+                '发布最新的攻击策略时,能够通过推送的方式下发到被保护的应用中去,让被防护对象处于最佳防护状态。',
+        },
+
+        {
+            img: 'http://101.200.41.205:8080/images/redesign/4.png',
+            title: '文件云分析',
+            dec:
+                '借助云端不断强化的后门分析数据库,可以轻松找出应用中可能存在的安全隐患和后门文件。',
+        },
+    ];
+    const Items = images.map((item, index) => {
+        return (
+            <div className='carousel_item' key={index}>
+                <div>
+                    <img src={item.img} alt={item.title} />
+                </div>
+                <h3>{item.title}</h3>
+                <p>{item.dec}</p>
+            </div>
+        );
+    });
+    return <Carousel>{Items}</Carousel>;
+};
 export const Login = () => {
     document.title = '登录页';
     const Logo = require('../assets/logo_anbai.png');
-    const LoginForm = Form.create({name: 'normal_login'})(FormEle);
+    const LoginForm = Form.create({ name: 'normal_login' })(FormEle);
     return (
         <div className='login'>
             {/* Logo */}
             <div className='logo'>
-                <img src={Logo} alt='Logo AnBai'/>
+                <img src={Logo} alt='Logo AnBai' />
             </div>
             {/* 登录框 */}
             <div className='content'>
-                <p className='head title'>客户授权系统</p>
-                <div className='box'>
-                    <div className='form-name'>
-                        <span>用户登录</span>
-                    </div>
-                    {/* 表单组件 */}
-                    <LoginForm/>
-                </div>
+                <Row gutter={30}>
+                    <Col lg={9} md={24} sm={24}>
+                        {/* <p className='head title'>客户授权系统</p> */}
+                        <div className='box'>
+                            <div className='form-name'>
+                                <span>用户登录</span>
+                            </div>
+                            {/* 表单组件 */}
+                            <LoginForm />
+                        </div>
+                    </Col>
+                    <Col lg={15} md={24} sm={24}>
+                        <CarouselEle />
+                    </Col>
+                </Row>
             </div>
         </div>
     );
